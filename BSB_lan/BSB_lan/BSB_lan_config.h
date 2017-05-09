b07fca001fd85c17b1c8bd17eead1b12980d8177
fredlcore <github@code-it.de>
2017-05-09 22:25:01 +0200

    Update BSB_lan_config.h

--- BSB_lan/BSB_lan/BSB_lan_config.h
+++ BSB_lan/BSB_lan/BSB_lan_config.h
@@ -116,8 +116,7 @@ int ipwe_parameters[] = {
 //#define RESET
 
 /*
- *  Enter a MAC address and IP address for your controller below.
- *  The IP address will be dependent on your local network:
+ *  Enter a MAC address, found either on the EthernetShield or use the one below.
 */
 byte mac[] = { 0x00, 0x80, 0x41, 0x19, 0x69, 0x90 };
 
