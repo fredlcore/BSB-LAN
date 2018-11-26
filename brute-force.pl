e8fda445c7f0eadae031a8cbec166b2c28d604f0
Frederik Holst <bsb@code-it.de>
2018-11-26 13:08:09 +0100

    Added brute-force detected parameters

---
+++ brute-force.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+
+my $baseID = 0x2D3D0000;
+my $counter = 0;
+my $ID = 0;
+my ($match, $answer);
+my $retries = 0;
+
+for ($counter; $counter < 65536; $counter++) {
+  $retries = 0;
+  $answer = "";
+  $ID = sprintf("%08X", $baseID + $counter);
+  $match = `grep "$ID" BSB_lan_defs.h`;
+  if ($? != 0) {
+    while ($answer le " " && $retries < 3) {
+      print STDERR "$ID\n";
+      $answer = `wget -q -O - http://192.168.1.50/4444/Y06,0x$ID | grep "DC 80 06"`;
+      if ($answer !~ /DC 80 06 0C 08/ && $answer gt " ") {
+        print "$ID\n";
+        print $answer . "\n";
+        print STDERR "$answer" . "\n";
+      }
+      sleep 0.25;
+      $retries++;
+      if ($retries > 1) {
+        print STDERR "Retrying...\n";
+      }
+    }
+  }
+}
