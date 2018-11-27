712baade7d7add315fdda8b1af549ded12457dfd
Frederik Holst <bsb@code-it.de>
2018-11-27 21:26:13 +0100

    Made script more configurable and added instructions

--- brute-force.pl
+++ brute-force.pl
@@ -1,7 +1,21 @@
 #!/usr/bin/perl
 use strict;
 
+# This script calls BSB-LAN and queries all possible cominations within one
+# major command area (defined in the fist byte of $baseID. The second byte
+# seems to be static or irrelevant and is mostly used as 3D which is why it is
+# hardcoded here.
+# To query other major command areas, change the 0x05... in $baseID to 
+# 0x2D (HK1), 0x31 (TWW) or others and report the results back to us.
+# 
+# Log output will be (overwritten) into brute-force-log.txt
+# Please note that BSB_lan_defs.h has to be in the same folder as this script.
+#
+# Set $URL to point to your BSB-LAN installation, including an active passkey.
+
+my $URL = "http://192.168.1.50/4444";
 my $baseID = 0x053D0000;
+
 my $counter = 0;
 my $ID = 0;
 my ($match, $answer);
@@ -10,6 +24,9 @@ my $retries = 0;
 local $/ = undef;
 open DEFS, "BSB_lan_defs.h";
 my $defs = <DEFS>;
+close DEFS;
+
+open LOG, ">./brute-force-log.txt";
 
 for ($counter; $counter < 65536; $counter++) {
   $retries = 0;
@@ -17,18 +34,19 @@ for ($counter; $counter < 65536; $counter++) {
   $ID = sprintf("%08X", $baseID + $counter);
   if ($defs !~ /$ID/) {
     while ($answer le " " && $retries < 3) {
-      print STDERR "$ID\n";
-      $answer = `wget -q -O - http://192.168.1.50/4444/Y06,0x$ID | grep "DC 80 0."`;
+      print "$ID\n";
+      $answer = `wget -q -O - $URL/Y06,0x$ID | grep "DC 80 0."`;
       if ($answer !~ /DC 80 0. 0C 08/ && $answer gt " ") {
-        print "$ID\n";
-        print $answer . "\n";
-        print STDERR "$answer" . "\n";
+        print LOG "$ID\n";
+        print LOG $answer . "\n";
+        print "$answer" . "\n";
       }
       sleep 0.25;
       $retries++;
       if ($retries > 1) {
-        print STDERR "Retrying...\n";
+        print "Retrying...\n";
       }
     }
   }
 }
+close LOG;
