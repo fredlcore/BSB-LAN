2ed01b993e3d8f83cf64c17a74ae3053b16a7141
Frederik Holst <bsb@code-it.de>
2018-11-27 21:34:35 +0100

    Made script more configurable and added instructions

--- brute-force.pl
+++ brute-force.pl
@@ -7,10 +7,17 @@ use strict;
 # hardcoded here.
 # To query other major command areas, change the 0x05... in $baseID to 
 # 0x2D (HK1), 0x31 (TWW) or others and report the results back to us.
+# Following major IDs exit: 05, 06, 07, 09, 0D, 11, 15, 19, 21, 22, 25, 29, 
+# 2A, 2B, 2D, 2E, 2F, 31, 49, 51, 59, 65, 69
+# Check in BSB_lan_defs.h first if a major group does apply to your heater
+# before running tests. Some of them are specific to certain features (such
+# as solar or cooling), so it won't make sense for you to test this group.
 # 
 # Log output will be (overwritten) into brute-force-log.txt
 # Please note that BSB_lan_defs.h has to be in the same folder as this script.
-#
+# Testing one major category will take approx. 18 hours, more if you increase
+# the delay between queries via the sleep command (now at 1/4 second).
+
 # Set $URL to point to your BSB-LAN installation, including an active passkey.
 
 my $URL = "http://192.168.1.50/4444";
