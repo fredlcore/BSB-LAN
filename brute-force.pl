cfb5ab55825c9d3ab4e446460bc8b125d7d132d5
Frederik Holst <bsb@code-it.de>
2018-11-27 16:58:13 +0100

    Added brute-force detected parameters

--- brute-force.pl
+++ brute-force.pl
@@ -7,16 +7,19 @@ my $ID = 0;
 my ($match, $answer);
 my $retries = 0;
 
+local $/ = undef;
+open DEFS, "BSB_lan_defs.h";
+my $defs = <DEFS>;
+
 for ($counter; $counter < 65536; $counter++) {
   $retries = 0;
   $answer = "";
   $ID = sprintf("%08X", $baseID + $counter);
-  $match = `grep "$ID" BSB_lan_defs.h`;
-  if ($? != 0) {
+  if ($defs !~ /$ID/) {
     while ($answer le " " && $retries < 3) {
       print STDERR "$ID\n";
-      $answer = `wget -q -O - http://192.168.1.50/4444/Y06,0x$ID | grep "DC 80 06"`;
-      if ($answer !~ /DC 80 06 0C 08/ && $answer gt " ") {
+      $answer = `wget -q -O - http://192.168.1.50/4444/Y06,0x$ID | grep "DC 80 0."`;
+      if ($answer !~ /DC 80 0. 0C 08/ && $answer gt " ") {
         print "$ID\n";
         print $answer . "\n";
         print STDERR "$answer" . "\n";
