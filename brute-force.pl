9d9688d857d5fd957182e342135d286114919106
Frederik Holst <bsb@code-it.de>
2019-01-22 22:37:21 +0100

    Added bus auto-detection and payload display

--- brute-force.pl
+++ brute-force.pl
@@ -24,6 +24,7 @@ use strict;
 
 my $URL = "http://192.168.1.50/4444";
 my $baseID = 0x05;
+my $param_nr = 11000;
 
 my $counter = 0;
 my $counter_padded = 0;
@@ -32,12 +33,35 @@ my ($match, $answer);
 my $retries = 0;
 $ID = $baseID * 0x1000000 + 0x3D0000;
 
+my $COID=0;
+my $len=0;
+my $payload_start=0;
+
+
+my $bus = `wget -q -O - $URL/C | grep Bus-System`;
+$bus =~ /.*: (.*) \((.*), (.*)\)/;
+$bus = $1;
+my $orig = $2;
+my $dest = $3;
+if ($bus ne "BSB" && $bus ne "LPB") {
+  print "Brute-force querying only works on BSB and LPB.\n";
+  exit;
+}
+
+my $heater = `wget -q -O - $URL/JQ=6224 | grep "value"`;
+$heater =~ /.*: "(.*)"/;
+$heater = $1;
+print "Running scan on $bus for Command ID range ";
+printf("%02X", $baseID);
+print " on $heater...\n";
+
 local $/ = undef;
 open DEFS, "BSB_lan_defs.h";
 my $defs = <DEFS>;
 close DEFS;
+$/ = "\n";
 
-open LOG, ">>./brute-force-log.txt";
+open LOG, ">>./defs-brute-force-$heater.h";
 
 for ($counter; $counter < 65536; $counter++) {
   $retries = 0;
@@ -47,11 +71,44 @@ for ($counter; $counter < 65536; $counter++) {
   if ($defs !~ /$baseID..$counter_padded/) {
     while ($answer le " " && $retries < 3) {
       print "$ID\n";
-      $answer = `wget -q -O - $URL/Y06,0x$ID | grep "DC 8. 0."`;
-      if ($answer !~ /DC 80 0. 0C 08/ && $answer !~ /DC 8. 0A/ && $answer gt " ") {
-        print LOG "$ID\n";
-        print LOG $answer . "\n";
-        print "$answer" . "\n";
+      if ($bus eq "BSB") {
+        $answer = `wget -q -O - $URL/Y06,0x$ID | grep "DC 8$dest 0$orig"`;
+      } else {
+        $answer = `wget -q -O - $URL/Y06,0x$ID | grep "0C 02 00 14"`;
+      }
+      if ((($bus eq "BSB" && $answer !~ /DC 8$dest 0$orig 0C 08/ && $answer !~ /DC 8. 0A/) || ($bus eq "LPB" && $answer =~ /02 00 14 .7/)) && $answer gt " ") {
+
+        my @msg = split(" ", $answer);
+        if ($bus eq "BSB") {
+          $len = hex($msg[3]) - 11;;
+          $COID = "$msg[5]$msg[6]$msg[7]$msg[8]";
+          $payload_start = 9;
+        } else {
+          $len = hex($msg[1]) - 14;
+          $COID = "$msg[9]$msg[10]$msg[11]$msg[12]";
+          $payload_start = 13;
+        }
+
+        my $payload = "";
+        my $x = 0;
+        for ($x=$payload_start;$x<$payload_start+$len;$x++) {
+          $payload .= " " . $msg[$x];
+        }
+
+        my $val = "";
+        my $temp = 0;
+        if ($len < 5) {
+          for ($x=$payload_start+1;$x<$payload_start+$len;$x++) {
+            $val .= $msg[$x];
+          }
+          $val = hex($val);
+          $temp = $val / 64;
+        }
+
+        print LOG "{0x$ID,  CAT_USER_DEFINED,     VT_UNKNOWN,       $param_nr, STR10200, 0,                    NULL,         FL_RONLY, DEV_ALL}, // Brute force detected Command ID via $bus, data payload on $heater:$payload (decimal: $val; temp: $temp)\n";
+
+        print $answer . "\n";
+        $param_nr++;
       }
       sleep 0.25;
       $retries++;
