c4cdcae6e298c21c08b0ef779eecdc6e95b64b39
Frederik Holst <bsb@code-it.de>
2019-12-11 18:03:16 +0100

    Updated version for DHT library

--- src/DHT/dht.h
+++ src/DHT/dht.h
@@ -1,9 +1,9 @@
 //
 //    FILE: dht.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.14
+// VERSION: 0.2.4
 // PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
-//     URL: http://arduino.cc/playground/Main/DHTLib
+//     URL: https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
 //
 // HISTORY:
 // see dht.cpp file
@@ -18,22 +18,22 @@
 #include <Arduino.h>
 #endif
 
-#define DHT_LIB_VERSION "0.1.14"
+#define DHT_LIB_VERSION "0.2.3 - dhtstable"
 
-#define DHTLIB_OK                0
-#define DHTLIB_ERROR_CHECKSUM   -1
-#define DHTLIB_ERROR_TIMEOUT    -2
-#define DHTLIB_INVALID_VALUE    -999
+const int DHTLIB_OK              = 0;
+const int DHTLIB_ERROR_CHECKSUM  = -1;
+const int DHTLIB_ERROR_TIMEOUT   = -2;
+const int DHTLIB_INVALID_VALUE   = -999;
 
-#define DHTLIB_DHT11_WAKEUP     18
-#define DHTLIB_DHT_WAKEUP       1
+const int DHTLIB_DHT11_WAKEUP    = 18;
+const int DHTLIB_DHT_WAKEUP      = 1;
 
-// max timeout is 100 usec.
-// For a 16 Mhz proc 100 usec is 1600 clock cycles
-// loops using DHTLIB_TIMEOUT use at least 4 clock cycli
+// max timeout is 100usec.
+// For a 16Mhz proc that is max 1600 clock cycles
+// loops using TIMEOUT use at least 4 clock cycli
 // so 100 us takes max 400 loops
 // so by dividing F_CPU by 40000 we "fail" as fast as possible
-#define DHTLIB_TIMEOUT (F_CPU/40000)
+const int DHTLIB_TIMEOUT = (F_CPU/40000);
 
 class dht
 {
@@ -42,20 +42,29 @@ public:
     // DHTLIB_OK
     // DHTLIB_ERROR_CHECKSUM
     // DHTLIB_ERROR_TIMEOUT
-    int read11(uint8_t pin);
-    int read(uint8_t pin);
+    int read11(uint8_t pin);      // DHT11 & DHT12
+    int read(uint8_t pin);        // DHT22
 
-    inline int read21(uint8_t pin) { return read(pin); };
-    inline int read22(uint8_t pin) { return read(pin); };
-    inline int read33(uint8_t pin) { return read(pin); };
-    inline int read44(uint8_t pin) { return read(pin); };
+    inline int read12(uint8_t pin)   { return read11(pin); }; // ok
+    inline int read21(uint8_t pin)   { return read(pin); };   // ok
+    inline int read22(uint8_t pin)   { return read(pin); };   // ok
+    inline int read33(uint8_t pin)   { return read(pin); };   // ok
+    inline int read44(uint8_t pin)   { return read(pin); };   // ok
+    inline int read2301(uint8_t pin) { return read(pin); };   // ok
+    inline int read2302(uint8_t pin) { return read(pin); };   // ok
+    inline int read2320(uint8_t pin) { return read(pin); };   //.ok
+    inline int read2322(uint8_t pin) { return read(pin); };   // ok
 
-    double humidity;
-    double temperature;
+    float humidity;
+    float temperature;
+
+    bool getDisableIRQ()              { return _disableIRQ; };
+    void setDisableIRQ(bool b )       { _disableIRQ = b; };
 
 private:
     uint8_t bits[5];  // buffer to receive data
-    int _readSensor(uint8_t pin, uint8_t wakeupDelay);
+    int     _readSensor(uint8_t pin, uint8_t wakeupDelay);
+    bool    _disableIRQ = false;
 };
 #endif
 //
