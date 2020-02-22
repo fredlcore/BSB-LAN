801fe269680032aa4c6a99b43784da6873c7c1d9
Serguei S. Dukachev <admin@mfitness.ru>
2020-02-22 21:35:38 +0300

    Update create-undefs.pl

--- localization/create-undefs.pl
+++ localization/create-undefs.pl
@@ -9,6 +9,31 @@ use bytes;
 $size[0] =~ /#define BUFLEN (.*)/;
 $size = $1-1;
 
+# Create LANG_C.h: language-independed definitions, based on German language
+$file = "LANG_DE.h";
+chomp($file);
+open( IN,   $file );
+open( OUT, ">LANG_C.h" );
+while ( $line = <IN> ) {
+    if ( !( $line =~ /^\/\// ) ) {
+        if ( $line =~ /#define (.*?) / ) {
+            $stringname = $1;
+            $stringname1 = $1;
+#            $stringname1 =~ s/_TEXT/_TXT/; # Save ~3 kB. Do not to change without discussion, please, because "_TEXT" used as marker in external interface.
+            print OUT "#define $stringname \"$stringname1\"\n";
+        }
+        else {
+            print OUT "$line";
+        }
+    }
+    else {
+        print OUT "$line";
+    }
+}
+close IN;
+close OUT;
+
+//create UNDEF files
 @files = `ls LANG*.h`;
 foreach $file (@files) {
   chomp ($file);
