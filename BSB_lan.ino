3bbb3e4c4daec602a6d8b4c8fe4f6bf4c305a1ab
Serguei S. Dukachev <github@dukess.ru>
2020-06-28 22:04:27 +0300

    Automatic creation of datalog.txt
    Automatic creation of datalog.txt on startup if it is not present on the SD card

--- BSB_lan.ino
+++ BSB_lan.ino
@@ -4543,6 +4543,20 @@ void resetBoard(){
   while (1==1) {}
 }
 
+#ifdef LOGGER
+boolean createdatalogFileAndWriteHeader(){
+  File dataFile = SD.open(datalogFileName, FILE_WRITE);
+  if (dataFile) {
+    strcpy_P(outBuf, PSTR("Milliseconds;Date;Parameter;Description;Value;Unit"));
+    dataFile.println(outBuf);
+    dataFile.close();
+    outBuf[0] = 0;
+    return true;
+  }
+  return false;
+}
+#endif
+
 /** *****************************************************************
  *  Function:
  *  Does:
@@ -6380,10 +6394,7 @@ uint8_t pps_offset = 0;
                 client.print(F(", "));
               }
               SD.remove(datalogFileName);
-              dataFile = SD.open(datalogFileName, FILE_WRITE);
-              if (dataFile) {
-                dataFile.println(F("Milliseconds;Date;Parameter;Description;Value;Unit"));
-                dataFile.close();
+              if (createdatalogFileAndWriteHeader()) {
                 filewasrecreated = true;
                 DebugOutput.print(datalogFileName);
                 client.print(datalogFileName);
@@ -7956,6 +7967,9 @@ for (int i=0; i<=LAST_ENUM_NR; i++) {
     avgfile.close();
   }
 
+if (!SD.exists(datalogFileName)) {
+  createdatalogFileAndWriteHeader();
+}
 #endif
 
   if (bus.getBusType() != BUS_PPS) {
