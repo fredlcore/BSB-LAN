e91971bac725de85069f2e373f779c0c8250313e
Serguei S. Dukachev <github@dukess.ru>
2020-12-16 16:00:42 +0300

    Update BSB_lan.ino
    small optimization in set()

--- BSB_lan.ino
+++ BSB_lan.ino
@@ -4224,11 +4224,7 @@ int set(int line      // the ProgNr of the heater parameter
     case VT_MINUTES:
       {
       uint32_t t = 0;
-      if (val[0] == '-') {
-        t=((int)(atof(val)*operand));
-      } else {
-        t=atoi(val)*operand;
-      }
+      t=atof(val)*operand;
       for (int x=payload_length;x>0;x--) {
         param[payload_length-x+1] = (t >> ((x-1)*8)) & 0xff;
       }
@@ -4318,7 +4314,7 @@ int set(int line      // the ProgNr of the heater parameter
     // Temperature values, mult=64
     case VT_TEMP:
       {
-      uint32_t t=((int)(atof(val)*operand));
+      uint32_t t=atof(val)*operand;
       if(setcmd){
         param[0]=enable_byte;
         param[1]=(t >> 8);
