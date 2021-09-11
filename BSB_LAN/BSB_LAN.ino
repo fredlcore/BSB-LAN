a54c0ab0719d43be698394cdbf59c6892ebb357e
Сергей С. Дукачёв <admin@mfitness.ru>
2021-09-12 00:42:53 +0300

    Olimex ESP32 EVB + Webserver

--- BSB_LAN/BSB_LAN.ino
+++ BSB_LAN/BSB_LAN.ino
@@ -6456,7 +6456,7 @@ void internalLEDBlinking(uint16_t period, uint16_t count) {
  * *************************************************************** */
 void loop() {
   
-#ifdef ESP32
+#if defined(WIFI) && defined(ESP32)
 //  esp_task_wdt_reset();
   // if WiFi is down, try reconnecting every 5 minutes
   if (WiFi.status() != WL_CONNECTED) {
@@ -7349,22 +7349,27 @@ uint8_t pps_offset = 0;
           }
           // if the file is available, read from it:
           if (dataFile) {
+            unsigned long filesize = dataFile.size();
+#if !defined(ESP32)
             dir_t d;
             uint16_t lastWrtYr = 0;
             byte monthval = 0;
             byte dayval = 0;
-            unsigned long filesize = dataFile.size();
             if (dataFile.dirEntry(&d)) {
               lastWrtYr = FAT_YEAR(d.lastWriteDate);
               monthval = FAT_MONTH(d.lastWriteDate);
               dayval = FAT_DAY(d.lastWriteDate);
               }
-
+#endif
             if ((httpflags & HTTP_ETAG))  { //Compare ETag if presented
+#if !defined(ESP32)
               if (memcmp(outBuf, outBuf + buffershift, sprintf_P(outBuf + buffershift, PSTR("\"%02d%02d%d%02d%02d%02d%lu\""), dayval, monthval, lastWrtYr, FAT_HOUR(d.lastWriteTime), FAT_MINUTE(d.lastWriteTime), FAT_SECOND(d.lastWriteTime), filesize))) {
                 // reuse httpflags
                 httpflags &= ~HTTP_ETAG; //ETag not match
               }
+#else
+                httpflags &= ~HTTP_ETAG; //ETag not match
+#endif
             }
 
             printToDebug(PSTR("File opened from SD: "));
@@ -7377,6 +7382,7 @@ uint8_t pps_offset = 0;
               code = HTTP_OK;
             }
             printHTTPheader(code, mimetype, HTTP_DO_NOT_ADD_CHARSET_TO_HEADER, (httpflags & HTTP_GZIP), HTTP_AUTO_CACHE_AGE);
+#if !defined(ESP32)
             if (lastWrtYr) {
               char monthname[4];
               char downame[4];
@@ -7392,6 +7398,9 @@ uint8_t pps_offset = 0;
             }
             //max-age=84400 = one day, max-age=2592000 = 30 days. Last string in header, double \r\n
             printFmtToWebClient(PSTR("ETag: \"%02d%02d%d%02d%02d%02d%lu\"\r\nContent-Length: %lu\r\n\r\n"), dayval, monthval, lastWrtYr, FAT_HOUR(d.lastWriteTime), FAT_MINUTE(d.lastWriteTime), FAT_SECOND(d.lastWriteTime), filesize, filesize);
+#else
+            printFmtToWebClient(PSTR("Content-Length: %lu\r\n\r\n"), filesize);
+#endif
             flushToWebClient();
             //Send file if !HEAD request received or ETag not match
             if (!(httpflags & HTTP_ETAG) && !(httpflags & HTTP_HEAD_REQ)) {
