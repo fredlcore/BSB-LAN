86caa379643739d96766b63fdb91702a0d61e8d6
Сергей С. Дукачёв <admin@mfitness.ru>
2021-09-24 12:08:48 +0300

    split .ino to multiple .h

---
+++ BSB_LAN/include/broadcast_msg_handling.h
@@ -0,0 +1,107 @@
+void broadcast_msg_handling(byte *msg){
+  // Is this a broadcast message?
+  if (((msg[2]==ADDR_ALL && bus->getBusType()==BUS_BSB) || (msg[2]>=0xF0 && bus->getBusType()==BUS_LPB)) && msg[4+(bus->getBusType()*4)]==TYPE_INF) { // handle broadcast messages
+  // Decode the rcv telegram and send it to the PC serial interface
+    if (!verbose && !monitor) {        // don't log twice if in verbose mode, but log broadcast messages also in non-verbose mode
+      printTelegram(msg, -1);
+      LogTelegram(msg);
+    }
+
+    // Filter Brenner Status messages
+
+    uint32_t cmd;
+    cmd=(uint32_t)msg[5+(bus->getBusType()*4)]<<24 | (uint32_t)msg[6+(bus->getBusType()*4)]<<16 | (uint32_t)msg[7+(bus->getBusType()*4)] << 8 | (uint32_t)msg[8+(bus->getBusType()*4)];
+    if (cmd==0x0500006C) {   // set Time from BC, same CommandID for BSB and LPB
+      setTime(msg[bus->getPl_start()+5], msg[bus->getPl_start()+6], msg[bus->getPl_start()+7], msg[bus->getPl_start()+3], msg[bus->getPl_start()+2], msg[bus->getPl_start()+1]+1900);
+    }
+
+    if (cmd==0x31000212) {    // TWW Status
+      printFmtToDebug(PSTR("INF: TWW-Status: %d\r\n"), msg[11]);
+
+      if ((msg[11] & 0x08) == 0x08) {  // See parameter 1602
+        if (TWW_start==0) {        // has not been timed
+          TWW_start=millis();   // keep current timestamp
+          TWW_count++;          // increment number of starts
+        }
+      } else {             // TWW Ladung aus
+        if (TWW_start!=0) {        // start has been timed
+          unsigned long TWW_end;
+          TWW_end=millis();      // timestamp the end
+          if (TWW_end >= TWW_start) {
+            TWW_duration+=(TWW_end-TWW_start)/1000;
+          } else { // overflow
+            TWW_duration+=(0xffffffff-TWW_start+TWW_end)/1000;
+          }
+            TWW_start=0;
+        } // endif, a previous start has been timed
+      } // endif, TWW is off
+    } // endif, Status TWW command code
+
+    if (cmd==0x05000213) {     // Brennerstatus; CommandID 0x053d0f66 was suggested at some point as well, but so far has not been identified in one of the heating systems
+      unsigned long brenner_end;
+      bool reset_brenner_timer = 0;
+      printFmtToDebug(PSTR("INF: Brennerstatus: %d\r\n"), msg[bus->getPl_start()]);      // first payload byte
+
+      if ((msg[bus->getPl_start()] & 0x04) == 0x04) {       // Stufe 1
+        if (brenner_start==0) {        // has not been timed
+          brenner_start=millis();   // keep current timestamp
+          brenner_count++;          // increment number of starts
+        }
+        if (brenner_stufe == 2) {
+          reset_brenner_timer = 1;
+        }
+        brenner_stufe=1;
+      }
+      if ((msg[bus->getPl_start()] & 0x10) == 0x10) {       // Stufe 2 (only oil furnace)
+        if (brenner_start_2==0) {        // has not been timed
+          brenner_start_2=millis();   // keep current timestamp
+          brenner_count_2++;          // increment number of starts
+        }
+        if (brenner_stufe == 1) {
+          reset_brenner_timer = 1;
+        }
+        brenner_stufe=2;
+      }
+      if (reset_brenner_timer == 1) {   // Stufenwechsel bei mehrstufigem Ölbrenner
+        brenner_end=millis();      // timestamp the end
+        if (brenner_stufe == 2) {    // Stufe jetzt 2, war also vorher 1
+          if (brenner_end >= brenner_start) {
+            brenner_duration+=(brenner_end-brenner_start)/1000;
+          } else { // overflow
+            brenner_duration+=(0xffffffff-brenner_start+brenner_end)/1000;
+          }
+          brenner_start=0;
+        }
+        if (brenner_stufe == 1) {   // Stufe jetzt 1, war also vorher 2
+          if (brenner_end >= brenner_start_2) {
+            brenner_duration_2+=(brenner_end-brenner_start_2)/1000;
+          } else { // overflow
+            brenner_duration_2+=(0xffffffff-brenner_start_2+brenner_end)/1000;
+          }
+          brenner_start_2=0;
+        }
+        reset_brenner_timer = 0;
+      }
+      if ((msg[bus->getPl_start()] & 0x04) != 0x04) {    // brenner off
+        brenner_end=millis();      // timestamp the end
+        brenner_stufe=0;
+        if (brenner_start!=0) {        // start has been timed
+          if (brenner_end >= brenner_start) {
+            brenner_duration+=(brenner_end-brenner_start)/1000;
+          } else { // overflow
+            brenner_duration+=(0xffffffff-brenner_start+brenner_end)/1000;
+          }
+          brenner_start=0;
+        } // endif, a previous start has been timed
+        if (brenner_start_2!=0) {        // start has been timed
+          if (brenner_end >= brenner_start_2) {
+            brenner_duration_2+=(brenner_end-brenner_start_2)/1000;
+          } else { // overflow
+            brenner_duration_2+=(0xffffffff-brenner_start_2+brenner_end)/1000;
+          }
+          brenner_start_2=0;
+        } // endif, a previous start has been timed
+      } // endif, brenner is off
+    } // endif, Status Brenner command code
+  } // endif, broadcasts
+}
---
+++ BSB_LAN/include/print_webpage.h
@@ -0,0 +1,192 @@
+
+/** *****************************************************************
+ *  Function:  webPrintHeader()
+ *  Does:      Sets up the HTML code to start a web page
+ *  Pass parameters:
+ *   none
+ *  Parameters passed back:
+ *   none
+ *  Function value returned:
+ *   none
+ *  Global resources used:
+ *   client object
+ * *************************************************************** */
+ void webPrintHeader(void) {
+   flushToWebClient();
+   printHTTPheader(HTTP_OK, MIME_TYPE_TEXT_HTML, HTTP_ADD_CHARSET_TO_HEADER, HTTP_FILE_NOT_GZIPPED, HTTP_DO_NOT_CACHE);
+ #if defined(__AVR__)
+   printPStr(pgm_get_far_address(header_html), sizeof(header_html));
+   #if !defined(I_DO_NOT_NEED_NATIVE_WEB_INTERFACE)
+   printPStr(pgm_get_far_address(header_html2), sizeof(header_html2));
+   #endif
+   printPStr(pgm_get_far_address(header_html3), sizeof(header_html3));
+ #else
+   printPStr(header_html, sizeof(header_html));
+   #if !defined(I_DO_NOT_NEED_NATIVE_WEB_INTERFACE)
+   printPStr(header_html2, sizeof(header_html2));
+   #endif
+   printPStr(header_html3, sizeof(header_html3));
+ #endif
+ #if !defined(I_DO_NOT_NEED_NATIVE_WEB_INTERFACE)
+   printToWebClient(PSTR("<a href='/"));
+   printPassKey();
+   printToWebClient(PSTR("' ID=main_link class='logo'>"));
+   printPStr(svg_favicon, sizeof(svg_favicon));
+//   printToWebClient(PSTR("<img width=10% height=10% src='/favicon.svg'></A>\r\n"));
+   printToWebClient(PSTR("</A></center>\r\n"));
+   printToWebClient(PSTR("<table align=center><tr bgcolor=#f0f0f0>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='/"));
+   printPassKey();
+   printToWebClient(PSTR("K'>" MENU_TEXT_HFK));
+
+   printToWebClient(PSTR("</a></td>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+   printToWebClient(PSTR("<a href='/"));
+   printPassKey();
+   printToWebClient(PSTR("K50'>"));
+   printToWebClient(STR_TEXT_SNS);
+
+   printToWebClient(PSTR("</a></td>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   if (!logCurrentValues) {
+     printToWebClient(PSTR("<font color=#000000>" MENU_TEXT_DLG "</font></td>"));
+   } else {
+     printToWebClient(PSTR("<a href='/"));
+     printPassKey();
+     printToWebClient(PSTR("DG'>" MENU_TEXT_SLG));
+     printToWebClient(PSTR("</a></td>"));
+   }
+
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='/"));
+   printPassKey();
+   printToWebClient(PSTR("Q' TARGET='_new'>" MENU_TEXT_CHK));
+   printToWebClient(PSTR("</a></td>"));
+
+   printToWebClient(PSTR("</tr>\r\n<tr bgcolor=#f0f0f0>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='/"));
+   printPassKey();
+   printToWebClient(PSTR("C'>" MENU_TEXT_CFG));
+
+   //  client.print(F("</a></td><td width=20% align=center><a href='http://github.com/fredlcore/bsb_lan/blob/master/command_ref/command_ref_" str(LANG) ".md'>" MENU_TEXT_URL));
+   printToWebClient(PSTR("</a></td>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='"));
+   printToWebClient(PSTR(MENU_LINK_URL "' target='_new'>" MENU_TEXT_URL));
+
+   printToWebClient(PSTR("</a></td>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='"));
+   printToWebClient(PSTR(MENU_LINK_TOC "' target='new'>" MENU_TEXT_TOC));
+   printToWebClient(PSTR("</a></td>"));
+   printToWebClient(PSTR("<td class=\"header\" width=20% align=center>"));
+
+   printToWebClient(PSTR("<a href='"));
+   printToWebClient(PSTR(MENU_LINK_FAQ "' target='_new'>" MENU_TEXT_FAQ));
+   printToWebClient(PSTR("</a></td>"));
+
+ //  client.println(F("<td width=20% align=center><a href='http://github.com/fredlcore/bsb_lan' target='new'>GitHub Repo</a></td>"));
+   printToWebClient(PSTR("</tr></table>"));
+#endif
+   printToWebClient(PSTR("<p></p><table align=center><tr><td class=\"header\">\r\n"));
+   flushToWebClient();
+ } // --- webPrintHeader() ---
+
+
+/** *****************************************************************
+ *  Function:  webPrintFooter()
+ *  Does:      Sets up the closing HTML code of a web page.
+ *  Pass parameters:
+ *   none
+ *  Parameters passed back:
+ *   none
+ *  Function value returned:
+ *   none
+ *  Global resources used:
+ *   client object
+ * *************************************************************** */
+void webPrintFooter(void) {
+  printToWebClient(PSTR("</td></tr></table>\r\n</body>\r\n</html>\r\n\r\n"));
+  flushToWebClient();
+} // --- webPrintFooter() ---
+
+/** *****************************************************************
+ *  Function:  webPrintSite()
+ *  Does:      Sets up HTML code to display a help page.
+ *  Pass parameters:
+ *   none
+ *  Parameters passed back:
+ *   none
+ *  Function value returned:
+ *   none
+ *  Global resources used:
+ *   client object
+ * *************************************************************** */
+void webPrintSite() {
+  webPrintHeader();
+
+  printlnToWebClient(PSTR("<p>"));
+  printToWebClient(PSTR("BSB-LAN, Version "));
+  printToWebClient(BSB_VERSION);
+  printlnToWebClient(PSTR("<p><b>" MENU_TEXT_HFK ":</b> " MENU_DESC_HFK ));
+  printlnToWebClient(PSTR("<p><b>" MENU_TEXT_CFG ":</b> " MENU_DESC_CFG ));
+  printlnToWebClient(PSTR("<p><b>" MENU_TEXT_URL ":</b> " MENU_DESC_URL ));
+
+#if !defined(__AVR__)
+#ifdef VERSION_CHECK
+  if (enable_version_check) {
+    printlnToWebClient(PSTR("<BR><BR>" MENU_TEXT_NVS "...<BR>"));
+    flushToWebClient();
+    httpclient.connect("bsb-lan.de", 80);
+    httpclient.println("GET /bsb-version.h\r\n");
+
+    unsigned long timeout = millis();
+    while (millis() - timeout < 3000 && !httpclient.available()) {
+    }
+
+    int major = -1;
+    int minor = -1;
+    int patch = -1;
+    char version_number[15] = { 0 };
+    while (httpclient.available()) {
+      char c = httpclient.read();
+      if (c == '\"') {
+        int index = 0;
+        do {
+          c = httpclient.read();
+          version_number[index] = c;
+          index++;
+        } while (c != '\"');
+        if (index > 1) {
+          version_number[index-1] = '\0';
+          if (major < 0) {
+            major = atoi(version_number);
+          } else if (minor < 0) {
+            minor = atoi(version_number);
+          } else if (patch < 0) {
+            patch = atoi(version_number);
+          }
+        }
+      }
+    }
+    httpclient.stop();
+    if ((major > atoi(MAJOR)) || (major == atoi(MAJOR) && minor > atoi(MINOR)) || (major == atoi(MAJOR) && minor == atoi(MINOR) && patch > atoi(PATCH))) {
+      printToWebClient(PSTR(MENU_TEXT_NVA ": "));
+      printFmtToWebClient(PSTR("<A HREF=\"https://github.com/fredlcore/BSB-LAN/archive/master.zip\">%d.%d.%d</A><BR>\r\n"), major, minor, patch);
+    } else {
+      printlnToWebClient(PSTR(MENU_TEXT_NVN));
+    }
+  }
+#endif
+#endif
+
+  webPrintFooter();
+} // --- webPrintSite() ---
