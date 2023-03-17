4c5f7a7864f267fe776fb3e3465895f9b8182e6a
-cr <105788188+DE-cr@users.noreply.github.com>
2023-03-17 20:00:22 +0100

    don't use 4 KB malloc'ed buffer in /Da,b on Arduino

--- BSB_LAN/BSB_LAN.ino
+++ BSB_LAN/BSB_LAN.ino
@@ -6395,7 +6395,10 @@ void loop() {
                         unsigned long nBytesToDo = datalogToPosition - datalogFromPosition;
                         // the following re-uses code fragments from transmitFile():
                         unsigned logbuflen = (OUTBUF_USEFUL_LEN + OUTBUF_LEN > 1024)?1024:(OUTBUF_USEFUL_LEN + OUTBUF_LEN);
-                        byte *buf = (byte*)malloc(4<<10);  // try to use 4 KB buffer, for improved transfer rates
+                        byte *buf = 0;
+#ifdef ESP32 // Arduino seems to have problems with the bigger, malloc'ed buffer
+                        buf = (byte*)malloc(4<<10);  // try to use 4 KB buffer, for improved transfer rates
+#endif
                         if (buf) logbuflen=4<<10; else buf=(byte*)bigBuff;  // fall back to static buffer, if necessary
                         while (nBytesToDo) {
                           int n = dataFile.read(buf, nBytesToDo<logbuflen ?nBytesToDo :logbuflen);
