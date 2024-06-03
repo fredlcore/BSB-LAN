65f7b96a2f25596d7bf8cc12d9c4cc32327aa7e9
fredlcore <github@code-it.de>
2024-06-04 01:34:26 +0800

    Adjustments for arduino-esp32 framework version 3.0.0

--- BSB_LAN/src/OneWireNg/src/platform/OneWireNg_ArduinoSAM.h
+++ BSB_LAN/src/OneWireNg/src/platform/OneWireNg_ArduinoSAM.h
@@ -15,8 +15,8 @@
 
 #include <assert.h>
 #include "Arduino.h"
-#include "OneWireNg_BitBang.h"
-#include "platform/Platform_TimeCritical.h"
+#include "../OneWireNg_BitBang.h"
+#include "Platform_TimeCritical.h"
 
 /*
  * If configured - internal Arduino-SAM pin status in updated
