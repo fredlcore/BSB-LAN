1c153dd4d6f4d1a18100457f6fe788d0bcc677f0
fredlcore <github@code-it.de>
2024-10-14 07:56:05 +0200

    Add setting average setpoint temperature to example

--- BSB_LAN/custom_functions/Send_room_temperature_based_on_MAX_thermostats/BSB_LAN_custom.h
+++ BSB_LAN/custom_functions/Send_room_temperature_based_on_MAX_thermostats/BSB_LAN_custom.h
@@ -8,10 +8,12 @@ if (custom_timer > custom_timer_compare+60000) {    // every 60 seconds
 
   max_cul->println("Zr"); // periodically (re-)activate "Moritz" mode on Cube to receive MAX messages
   int max_avg_count = 0;
-  float max_avg = 0;
+  float max_avg_cur = 0;
+  float max_avg_dst = 0;
   for (int x=0;x<20;x++) {
     if (max_cur_temp[x] > 0) {
-      max_avg += (float)max_cur_temp[x] / 10;
+      max_avg_cur += (float)max_cur_temp[x] / 10;
+      max_avg_dst += (float)max_dst_temp[x] / 2;
       max_avg_count++;
 /*
       // Debug output
@@ -26,13 +28,20 @@ if (custom_timer > custom_timer_compare+60000) {    // every 60 seconds
     }
   }
   if (max_avg_count > 0) {
-    Serial.print("Avgerage temperature based on all MAX! thermostats: ");
-    Serial.println(max_avg / max_avg_count);
+    Serial.print("Avgerage current temperature based on all MAX! thermostats: ");
+    Serial.println(max_avg_cur / max_avg_count);
+    Serial.print("Avgerage setpoint temperature based on all MAX! thermostats: ");
+    Serial.println(max_avg_dst / max_avg_count);
 
     char set_temp[6];
-    dtostrf((max_avg/max_avg_count), 1, 1, set_temp);   // Convert temperature value to char as required by set() function
+    dtostrf((max_avg_cur/max_avg_count), 1, 1, set_temp);   // Convert current temperature value to char as required by set() function
     Serial.print("Setting room temperature to ");
     Serial.println(set_temp);
     set(10000, set_temp, 0);
+
+    dtostrf((max_avg_dst/max_avg_count), 1, 1, set_temp);   // Convert setpoint temperature value to char as required by set() function
+    Serial.print("Setting setpoint temperature to ");
+    Serial.println(set_temp);
+    set(710, set_temp, 0);                                  // Write average setpoint temperature to parameter 710 (comfort setpoint HC1, adjust parameter as necessary)
   }
 }
