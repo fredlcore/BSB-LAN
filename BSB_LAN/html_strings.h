9e9198702f93da65d577f115d377ce5d3149a4f6
fredlcore <github@code-it.de>
2024-11-28 00:42:19 +0800

    Remove deprecated underscore and empty value when setting parameter

--- BSB_LAN/html_strings.h
+++ BSB_LAN/html_strings.h
@@ -430,9 +430,9 @@ const char header_html[] =
   "</head><body>";
 const char header_html2[] =
   "<script>function set(p,d){" NEWLINE
-    "var v=document.getElementById('value'+p+'-'+d).value.replace(/\\.$/,'')" NEWLINE
-          ".replaceAll(' ','_')" NEWLINE
-          ".replace('---','');" NEWLINE
+    "var v=document.getElementById('value'+p+'-'+d).value.replace(/\\.$/,'');" NEWLINE
+//          ".replaceAll(' ','_')" NEWLINE
+//          ".replace('---','');" NEWLINE
     "window.open(document.getElementById('main_link').href+'S'+p+'!'+d+'='+v,'_self')" NEWLINE
   "}" NEWLINE
   "function setbit(p){" NEWLINE
