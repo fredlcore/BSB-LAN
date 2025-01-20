5536039e4ae616bb5f5ffb0ab371b0182e569ba8
fredlcore <github@code-it.de>
2025-01-20 11:38:32 +0800

    Cleanup

--- BSB_LAN/src/BSB/bsb.cpp
+++ BSB_LAN/src/BSB/bsb.cpp
@@ -1,12 +1,12 @@
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+  #include "Arduino.h"
 #else
-#include "WProgram.h"
+  #include "WProgram.h"
 #endif
 #if defined(ESP32)
-#include "driver/uart.h"
-#include "soc/uart_struct.h"
-#include "soc/uart_reg.h"
+  #include "driver/uart.h"
+  #include "soc/uart_struct.h"
+  #include "soc/uart_reg.h"
 #endif
 
 #include "bsb.h"
