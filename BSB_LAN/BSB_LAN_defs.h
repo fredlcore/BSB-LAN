1bdcaa50aaeb8c0855818e5148087f4b810c3e50
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2025-05-20 21:50:13 +0100

    Revert Indenting Changes in BSB_LAN_defs.h

--- BSB_LAN/BSB_LAN_defs.h
+++ BSB_LAN/BSB_LAN_defs.h
@@ -1407,32 +1407,32 @@ const char ENUM15046[] = {
 {0xDEADBEEF,  VT_UNKNOWN,       19999, STR99999, 0,                    NULL,         DEFAULT_FLAG, DEV_ALL}, //
 
 
-{CMD_UNKNOWN, VT_SECONDS_DWORD,     BSP_INTERNAL+0, STR20000, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 1 runtime
-{CMD_UNKNOWN, VT_DWORD,             BSP_INTERNAL+1, STR20001, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 1 starts
-{CMD_UNKNOWN, VT_SECONDS_DWORD,     BSP_INTERNAL+2, STR20002, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 2 runtime
-{CMD_UNKNOWN, VT_DWORD,             BSP_INTERNAL+3, STR20003, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 2 starts
-{CMD_UNKNOWN, VT_SECONDS_DWORD,     BSP_INTERNAL+4, STR20004, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHW runtime
-{CMD_UNKNOWN, VT_DWORD,             BSP_INTERNAL+5, STR20005, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHW starts
-{CMD_UNKNOWN, VT_ONOFF,             BSP_INTERNAL+6, STR20006, sizeof(ENUM_ONOFF), ENUM_ONOFF,   DEFAULT_FLAG, DEV_ALL}, // reset 20000-20005
-{CMD_UNKNOWN, VT_FLOAT,             BSP_AVERAGES,   STR20000, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // Dummy for averages parameters
-{CMD_UNKNOWN, VT_STRING,            (float)BSP_DHT22+0.0,  STR20100, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor ID
-{CMD_UNKNOWN, VT_TEMP,              (float)BSP_DHT22+0.1,  STR20101, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor Current temperature
+{CMD_UNKNOWN, VT_SECONDS_DWORD, BSP_INTERNAL+0, STR20000, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 1 runtime
+{CMD_UNKNOWN, VT_DWORD,         BSP_INTERNAL+1, STR20001, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 1 starts
+{CMD_UNKNOWN, VT_SECONDS_DWORD, BSP_INTERNAL+2, STR20002, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 2 runtime
+{CMD_UNKNOWN, VT_DWORD,         BSP_INTERNAL+3, STR20003, 0,                  NULL,         FL_RONLY, DEV_ALL},     // Burner stage 2 starts
+{CMD_UNKNOWN, VT_SECONDS_DWORD, BSP_INTERNAL+4, STR20004, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHW runtime
+{CMD_UNKNOWN, VT_DWORD,         BSP_INTERNAL+5, STR20005, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHW starts
+{CMD_UNKNOWN, VT_ONOFF,         BSP_INTERNAL+6, STR20006, sizeof(ENUM_ONOFF), ENUM_ONOFF,   DEFAULT_FLAG, DEV_ALL}, // reset 20000-20005
+{CMD_UNKNOWN, VT_FLOAT,         BSP_AVERAGES,   STR20000, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // Dummy for averages parameters
+{CMD_UNKNOWN, VT_STRING,        (float)BSP_DHT22+0.0,  STR20100, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor ID
+{CMD_UNKNOWN, VT_TEMP,          (float)BSP_DHT22+0.1,  STR20101, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor Current temperature
 {CMD_UNKNOWN, VT_RELHUMIDITY_WORD1, (float)BSP_DHT22+0.2,  STR20102, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor Humidity
-{CMD_UNKNOWN, VT_GR_PER_CUBM,       (float)BSP_DHT22+0.3,  STR20103, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor Abs Humidity
-{CMD_UNKNOWN, VT_STRING,            (float)BSP_BME280+0.0, STR20200, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor address/ID
-{CMD_UNKNOWN, VT_TEMP,              (float)BSP_BME280+0.1, STR20201, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Current temperature
+{CMD_UNKNOWN, VT_GR_PER_CUBM,   (float)BSP_DHT22+0.3,  STR20103, 0,                  NULL,         FL_RONLY, DEV_ALL},     // DHT22 sensor Abs Humidity
+{CMD_UNKNOWN, VT_STRING,        (float)BSP_BME280+0.0, STR20200, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor address/ID
+{CMD_UNKNOWN, VT_TEMP,          (float)BSP_BME280+0.1, STR20201, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Current temperature
 {CMD_UNKNOWN, VT_RELHUMIDITY_WORD1, (float)BSP_BME280+0.2, STR20202, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Humidity
-{CMD_UNKNOWN, VT_PRESSURE_HPA,      (float)BSP_BME280+0.3, STR20203, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Pressure [hPa]
-{CMD_UNKNOWN, VT_ALTITUDE,          (float)BSP_BME280+0.4, STR20204, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Altitude [m]
-{CMD_UNKNOWN, VT_GR_PER_CUBM,       (float)BSP_BME280+0.5, STR20205, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Abs Humidity
-{CMD_UNKNOWN, VT_STRING,            (float)BSP_ONEWIRE+0.0,STR20300, 0,                  NULL,         FL_RONLY, DEV_ALL},     // One wire (Dallas) sensor ID
-{CMD_UNKNOWN, VT_TEMP,              (float)BSP_ONEWIRE+0.1,STR20301, 0,                  NULL,         FL_RONLY, DEV_ALL},     // One wire (Dallas) sensor Current temperature
-{CMD_UNKNOWN, VT_STRING,            (float)BSP_MAX+0.0,    STR20500, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor ID
-{CMD_UNKNOWN, VT_TEMP,              (float)BSP_MAX+0.1,    STR20501, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor Current temperature
-{CMD_UNKNOWN, VT_TEMP,              (float)BSP_MAX+0.2,    STR20502, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL},     // MAX! sensor Destination temperature
-{CMD_UNKNOWN, VT_PERCENT_WORD1,     (float)BSP_MAX+0.3,    STR20503, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor valve opening (in percent)
-{CMD_UNKNOWN, VT_FLOAT,             BSP_FLOAT,             STR20700, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // custom_floats
-{CMD_UNKNOWN, VT_LONG,              BSP_LONG,              STR20800, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // custom_longs
+{CMD_UNKNOWN, VT_PRESSURE_HPA,  (float)BSP_BME280+0.3, STR20203, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Pressure [hPa]
+{CMD_UNKNOWN, VT_ALTITUDE,      (float)BSP_BME280+0.4, STR20204, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Altitude [m]
+{CMD_UNKNOWN, VT_GR_PER_CUBM,   (float)BSP_BME280+0.5, STR20205, 0,                  NULL,         FL_RONLY, DEV_ALL},     // BME280 sensor Abs Humidity
+{CMD_UNKNOWN, VT_STRING,        (float)BSP_ONEWIRE+0.0,STR20300, 0,                  NULL,         FL_RONLY, DEV_ALL},     // One wire (Dallas) sensor ID
+{CMD_UNKNOWN, VT_TEMP,          (float)BSP_ONEWIRE+0.1,STR20301, 0,                  NULL,         FL_RONLY, DEV_ALL},     // One wire (Dallas) sensor Current temperature
+{CMD_UNKNOWN, VT_STRING,        (float)BSP_MAX+0.0,    STR20500, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor ID
+{CMD_UNKNOWN, VT_TEMP,          (float)BSP_MAX+0.1,    STR20501, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor Current temperature
+{CMD_UNKNOWN, VT_TEMP,          (float)BSP_MAX+0.2,    STR20502, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL},     // MAX! sensor Destination temperature
+{CMD_UNKNOWN, VT_PERCENT_WORD1, (float)BSP_MAX+0.3,    STR20503, 0,                  NULL,         FL_RONLY, DEV_ALL},     // MAX! sensor valve opening (in percent)
+{CMD_UNKNOWN, VT_FLOAT,         BSP_FLOAT,             STR20700, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // custom_floats
+{CMD_UNKNOWN, VT_LONG,          BSP_LONG,              STR20800, 0,                  NULL,         DEFAULT_FLAG, DEV_ALL}, // custom_longs
 
 //{CMD_END,     VT_UNKNOWN,       65535, "",       0,                    NULL,         DEFAULT_FLAG, DEV_ALL}
 
