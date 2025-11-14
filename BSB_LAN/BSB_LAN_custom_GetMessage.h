9bce63b55c4913e9d7ae5d99b7db36a689701fbe
fredlcore <github@code-it.de>
2025-11-14 10:25:48 +0100

    Moved example to custom_functions

--- BSB_LAN/BSB_LAN_custom_GetMessage.h
+++ BSB_LAN/BSB_LAN_custom_GetMessage.h
@@ -1,15 +1,3 @@
-// Is this a broadcast message?
-if (((msg[2]==ADDR_ALL && bus->getBusType()==BUS_BSB) || ((msg[2] & 0x0F) == 0x0F && bus->getBusType()==BUS_LPB)) && msg[4+(bus->offset)]==TYPE_INF) { // Only handle broadcast messages
-  uint32_t cmd=(uint32_t)msg[5+(bus->offset)]<<24 | (uint32_t)msg[6+(bus->offset)]<<16 | (uint32_t)msg[7+(bus->offset)] << 8 | (uint32_t)msg[8+(bus->offset)];
-  if (cmd==0x0500006B) {
-    uint8_t error = msg[9+(bus->offset)];
-    if (error > 0) {
-      // Take note that the heating system will send error messages approx. once a minute until it is resolved.
-      printFmtToDebug("Heating system reports error no. %d occurred!\r\n", msg[9+(bus->offset)]); // First byte of payload of 0x0500006B is the error code
-      // Take some kind of action, such as notification, that goes beyond the common (MQTT-)handling of broadcast messages.
-    } else {
-      // Take note that it might happen that BSB-LAN doesn't catch the error == 0 broadcast
-      printFmtToDebug("Heating system reports that previous error has been resolved.\r\n");
-    }
-  }
-}
+// Add custom code for handling broadcast and other incoming messages here which will be included in the loop() function 
+// after the GetMessage() function call. If msg[0] is zero, no valid message has been received. Otherwise, you can process the 
+// received message as you like. See custom_functions/Monitor_error_broadcasts/BSB_LAN_custom_GetMessage.h for an example implementation.
