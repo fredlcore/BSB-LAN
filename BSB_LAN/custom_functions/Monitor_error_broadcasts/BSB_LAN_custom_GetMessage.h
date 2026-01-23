c7ce0d0bcc49049a595766f519e3677544c60a2a
fredlcore <github@code-it.de>
2026-01-23 22:55:42 +0100

    Added priority field in debug message.

--- BSB_LAN/custom_functions/Monitor_error_broadcasts/BSB_LAN_custom_GetMessage.h
+++ BSB_LAN/custom_functions/Monitor_error_broadcasts/BSB_LAN_custom_GetMessage.h
@@ -5,7 +5,7 @@ if (((msg[2]==ADDR_ALL && bus->getBusType()==BUS_BSB) || (msg[2] >= 0xF0 && bus-
     uint8_t error = msg[9+(bus->offset)];
     if (error > 0) {
       // Take note that the heating system will send error messages approx. once a minute until it is resolved.
-      printFmtToDebug("Heating system reports error no. %d occurred!\r\n", msg[9+(bus->offset)]); // First byte of payload of 0x0500006B is the error code
+      printFmtToDebug("Heating system reports error no. %d with priority %d occurred!\r\n", msg[9+(bus->offset)], msg[10+(bus->offset)]); // First byte of payload of 0x0500006B is the error code
       // Take some kind of action, such as notification, that goes beyond the common (MQTT-)handling of broadcast messages.
     } else {
       // Take note that it might happen that BSB-LAN doesn't catch the error == 0 broadcast
